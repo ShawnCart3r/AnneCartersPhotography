6c4baa811a89158728fba1a90178ceb02d18c7f5
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2020-10-11 13:31:43 -0400

    Add files via upload

---
+++ fullpage.js
@@ -0,0 +1,10 @@
+new fullpage('#fullPage', {
+    autoscrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3','section4', 'section5'],
+    navigationTooltips: ['Home', 'About', 'Barns', 'Covered Bridges', 'Rock Art'],
+    showActiveTooltip: true,
+    scrollingSpeed: 1000,
+   
+
+})
